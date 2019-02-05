a631632ce4f80cca205d9be9fd9b31a766811c83
Unknown <javi_vavi@hotmail.com>
2019-02-05 16:38:57 +0100

    Login Implementation Part 1

--- src/pages/login/login.component.ts
+++ src/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
 
   ngOnInit() {
+  }
+
+  private createForm(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    })
+  }
+
+  public submit(): void {
+    const {email, password} = this.loginForm.value;
+    console.log(`Email: ${email}, Password: ${password}`);
   }
 
 }
