1ffa9c216e7d542c93787fc3075db739c265a069
Unknown <javi_vavi@hotmail.com>
2019-02-07 17:36:11 +0100

    Chat Components Implementation  Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { ReactiveFormsModule } from '@angular/forms';
 import { NavbarComponent } from './components/navbar/navbar.component';
+
 import { ChatComponent } from './pages/chat/chat.component';
+import { ChatInputComponent } from './pages/chat/components/chat-input/chat-input.component';
+import { ChatroomListComponent } from './pages/chat/components/chatroom-list/chatroom-list.component';
+import { ChatroomTitleBarComponent } from './pages/chat/components/chatroom-title-bar/chatroom-title-bar.component';
+import { ChatMessageComponent } from './pages/chat/components/chat-message/chat-message.component';
+import { ChatroomWindowComponent } from './pages/chat/components/chatroom-window/chatroom-window.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +23,12 @@ import { ChatComponent } from './pages/chat/chat.component';
     LoginComponent,
     SignupComponent,
     NavbarComponent,
-    ChatComponent
+    ChatComponent,
+    ChatInputComponent,
+    ChatroomListComponent,
+    ChatroomTitleBarComponent,
+    ChatMessageComponent,
+    ChatroomWindowComponent
   ],
   imports: [
     BrowserModule,
