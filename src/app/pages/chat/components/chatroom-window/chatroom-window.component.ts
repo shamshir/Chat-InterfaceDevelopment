07232335c42b51ca2d523c221b0a4e48fed12d9d
Unknown <javi_vavi@hotmail.com>
2019-02-07 18:27:24 +0100

    Classes User & Message Added

--- src/app/pages/chat/components/chatroom-window/chatroom-window.component.ts
+++ src/app/pages/chat/components/chatroom-window/chatroom-window.component.ts
@@ -7,6 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatroomWindowComponent implements OnInit {
 
+  public dummyData = [
+    {
+      message: 'Sed enim velit, condimentum nec tincidunt non, elementum sed nisi.',
+      createdAt: new Date(),
+      sender: {
+        firstName: 'Sheldon',
+        lastName: 'Cooper',
+        photoUrl: 'http://placekitten.com/g/150/150'
+      }
+    },
+    {
+      message: 'Quisque ornare dapibus convallis. Nam tempor dui a nisl lobortis, sed gravida lectus laoreet. Nullam ornare dui magna. Duis in nisi libero.',
+      createdAt: new Date(),
+      sender: {
+        firstName: 'Leonard',
+        lastName: 'Hofstadter',
+        photoUrl: 'https://placekitten.com/g/150/150'
+      }
+    },
+    {
+      message: 'Ut eu elit sodales leo ultricies pulvinar. Fusce iaculis magna gravida tempus congue. Ut sit amet nulla sed nisi cursus mattis quis at lacus. Proin commodo, justo in elementum scelerisque, sem urna vulputate enim, ac posuere purus diam ac velit. Sed enim velit, condimentum nec tincidunt non, elementum sed nisi. Cras pharetra dui eu scelerisque pharetra. Curabitur auctor feugiat nibh eget molestie. Duis scelerisque auctor mi, sit amet efficitur magna vulputate quis. Quisque ornare dapibus convallis. Nam tempor dui a nisl lobortis, sed gravida lectus laoreet. Nullam ornare dui magna. Duis in nisi libero. Praesent eu tristique felis. Nunc vestibulum enim et justo dignissim lacinia nec et diam.',
+      createdAt: new Date(),
+      sender: {
+        firstName: 'Howard',
+        lastName: 'Wolowitz',
+        photoUrl: 'https://placekitten.com/g/150/150'
+      }
+    },
+    {
+      message: 'Quisque ornare dapibus convallis. Nam tempor dui a nisl lobortis, sed gravida lectus laoreet. Nullam ornare dui magna. Duis in nisi libero.',
+      createdAt: new Date(),
+      sender: {
+        firstName: 'Amy',
+        lastName: ' Farrah Fowler',
+        photoUrl: 'https://placekitten.com/g/150/150'
+      }
+    }
+  ]; 
+
   constructor() { }
 
   ngOnInit() {
