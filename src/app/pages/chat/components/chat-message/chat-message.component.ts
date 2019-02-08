e4b15bd374c2d1d6e1618a8c0a77ae70ad233829
Unknown <javi_vavi@hotmail.com>
2019-02-08 19:14:44 +0100

    Chat Messages Working Part 1

--- src/app/pages/chat/components/chat-message/chat-message.component.ts
+++ src/app/pages/chat/components/chat-message/chat-message.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Message } from 'src/app/classes/message';
+import { Message } from '../../../../classes/message';
 
 @Component({
   selector: 'app-chat-message',
